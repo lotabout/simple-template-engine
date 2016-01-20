53dc83c94bff3311a20f0404090dd09dbf3ff8ff
zhang_ji <zhang_ji@worksap.co.jp>
2016-01-20 14:01:07 +0800

    basic template structure

--- src/template.py
+++ src/template.py
@@ -37,3 +37,53 @@ class CodeBuilder(object):
         global_namespace = {}
         exec(code, global_namespace)
         return global_namespace
+
+class Template():
+    """Create a Template"""
+    def __init__(self, text, *contexts):
+        self.text = text
+        self.context = {}
+        for context in contexts:
+            self.context.update(context)
+
+        self.all_vars = set()
+
+        code_builder = CodeBuilder()
+        code_builder.emit_line('def render_function(context, dot_func):')
+        code_builder.indent()
+        var_defs = code_builder.add_section()
+        code_builder.emit_line('result = []')
+        code_builder.emit_line('extend_result = result.extend')
+        code_builder.emit_line('append_result = result.append')
+
+        buffered = []
+        def flush_output():
+            """emit output string according to buffer"""
+            if len(buffered) == 1:
+                code_builder.emit_line('append_result({})'.format(buffered[0]))
+            elif len(buffered) > 1:
+                code_builder.emit_line('extend_result([{}])'.format(','.join(buffered)))
+            del buffered[:]
+
+        # compile the template_string into codes
+        tokens = re.split(r'(?s)({{.*?}}|{%.*?%}|{#.*?#})', self.text)
+
+        for token in tokens:
+            if token.startswith('{#'):
+                continue
+            elif token.startswith('{{'):
+                expr = self._expr_code(token[2:-2].strip())
+                buffered.append('to_str({})'.format(expr))
+            elif token.startswith('{%'):
+                pass
+            else:
+                buffered.append(token)
+
+        flush_output()
+
+        # add variables
+
+        # post process
+        code_builder.emit_line('return ''.join(result)')
+        code_builder.dedent()
+        self._render_function = code_builder.get_globals()['render_function']
